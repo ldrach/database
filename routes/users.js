325f1cd7c99da740ecdfb3dbaa1e72b619364eda
ldrach <40499729+ldrach@users.noreply.github.com>
2020-11-15 12:40:50 -0800

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+let user = require('../models/users.model');
+
+router.route('/').get((req, res) => {
+    user.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+
+    const newUser = new user({
+        username,
+        password,
+        firstname,
+        lastname,
+        email,
+    })
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    user.findById(req.params.id)
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    user.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    user.findById(req.params.id)
+        .then(users => {
+            users.username = req.body.username;
+            users.password = req.body.password;
+            users.firstname = req.body.password;
+            users.lastname = req.body.password;
+            users.email = req.body.email;
+
+            users.save()
+                .then(() => res.json('User updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
